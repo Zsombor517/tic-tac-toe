f1243f1fb607b9320c4ca95f6d96b2414596f1eb
Zsombor Csongradi <zombiewna@gmail.com>
2025-12-17 12:31:58 +0100

    Commit

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
 import promptSync from 'prompt-sync';
-const prompt = promptSync({sigint: true});
+const prompt = promptSync({ sigint: true });
 
 let board = getEmptyBoard();
 let playerTurn = 'x';
 let aiEnabled;
 
-while (true){
+while (true) {
     console.log('1. Two player mode\n2.Play against ai')
-    const input = prompt(); 
-    if (input == 1){
+    const input = prompt();
+    if (input == 1) {
         break;
     }
-    else if (input == 2){
+    else if (input == 2) {
         aiEnabled = true;
         break;
     }
@@ -19,108 +19,108 @@ while (true){
 }
 displayBoard(board);
 
-while (true){
+while (true) {
     let coords;
-    if (aiEnabled && playerTurn == 'o'){
+    if (aiEnabled && playerTurn == 'o') {
         console.log(getUnbeatableAiCoords(board));
-        coords = getRandomAiCoords(board);
+        coords = getUnbeatableAiCoords(board);
         board[coords[0]][coords[1]] = 'o';
         playerTurn = 'x';
         console.log("Ai turn: ")
     }
-    
-    else if (playerTurn == 'x'){
+
+    else if (playerTurn == 'x') {
         coords = getHumanCoordinates(board, playerTurn);
-        if (!coords) {break;}
+        if (!coords) { break; }
         board[coords[0]][coords[1]] = 'x';
         playerTurn = 'o';
     }
-    else if (!aiEnabled && playerTurn == 'o'){
+    else if (!aiEnabled && playerTurn == 'o') {
         coords = getHumanCoordinates(board, playerTurn);
-        if (!coords) {break;}
+        if (!coords) { break; }
         board[coords[0]][coords[1]] = 'o';
         playerTurn = 'x';
     }
     displayBoard(board);
-    if (isBoardFull(board)){
+    if (isBoardFull(board)) {
         console.log('Tie!');
         break;
     }
-    if (getWinner(board)){
+    if (getWinner(board)) {
         console.log(`The winners is ${getWinner(board)}`);
         break;
     }
 }
 
-function getEmptyBoard(){
-    return [['o', 'o', '.'], ['.', '.', '.'], ['.', '.', '.']];
+function getEmptyBoard() {
+    return [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']];
 }
 
-function displayBoard(board){
+function displayBoard(board) {
     console.log('  1 2 3');
     let row = 'A ';
-    for (const column of board[0]){
+    for (const column of board[0]) {
         row += `${column}|`
     }
     console.log(row);
 
     row = 'B ';
-    for (const column of board[1]){
+    for (const column of board[1]) {
         row += `${column}|`
     }
-    console.log('----+---+---\n' + row);
+    console.log('---+---+---\n' + row);
 
     row = 'C ';
-    for (const column of board[2]){
+    for (const column of board[2]) {
         row += `${column}|`
     }
-    console.log('----+---+---\n' + row);
+    console.log('---+---+---\n' + row);
 }
 
-function getHumanCoordinates(board, playerTurn){
+function getHumanCoordinates(board, playerTurn) {
     const rows = {
         a: 1,
         b: 2,
         c: 3
     }
-    while (true){
+    while (true) {
         let coords = prompt(`Player ${playerTurn} (Eg. a1): `);
-        if (coords.toLowerCase() == 'quit'){
+        if (coords.toLowerCase() == 'quit') {
             return false;
         }
-        else if (!/^[a-cA-C][1-3]$/.test(coords)){
+        else if (!/^[a-cA-C][1-3]$/.test(coords)) {
             console.log('Invalid move!');
         }
-        else{
+        else {
             coords = [rows[coords[0].toLowerCase()] - 1, parseInt(coords[1]) - 1];
-            if (board[coords[0]][coords[1]] != '.'){
+            if (board[coords[0]][coords[1]] != '.') {
                 console.log('Space already occupied!');
             }
-            else{ return coords; }
-        }    
-    }   
-}
-
-function isBoardFull(board){
-    let fullRowCounter = 0;
-    for (const row of board){
-        if (!row.includes('.')){
-            fullRowCounter ++;
+            else { return coords; }
         }
     }
-    if (fullRowCounter == 3){
+}
+
+function isBoardFull(board) {
+    let fullRowCounter = 0;
+    for (const row of board) {
+        if (!row.includes('.')) {
+            fullRowCounter++;
+        }
+    }
+    if (fullRowCounter == 3) {
         return true;
     }
     return false;
 }
 
-function getWinner(board){
+function getWinner(board) {
     //three in a row
-    for (const row of board){
-        if (!row.includes('.') && !row.includes('o')){
+    for (const row of board) {
+        if (!row.includes('.') && !row.includes('o')) {
             return 'x';
         }
-        else if (!row.includes('.') && !row.includes('x')){
+        else if (!row.includes('.') && !row.includes('x')) {
             return 'o';
         }
     }
@@ -128,20 +128,20 @@ function getWinner(board){
 
     let xCount = 0;
     let oCount = 0;
-    for (let i = 0; i <= board.length; i++){
-        for (const row of board){
-            if (row[i] == 'x'){
-                xCount ++;
+    for (let i = 0; i <= board.length; i++) {
+        for (const row of board) {
+            if (row[i] == 'x') {
+                xCount++;
             }
-            else if (row[i] == 'o'){
-                oCount ++;
+            else if (row[i] == 'o') {
+                oCount++;
             }
         }
 
-        if (xCount == 3){
+        if (xCount == 3) {
             return 'x';
         }
-        else if (oCount == 3){
+        else if (oCount == 3) {
             return 'o';
         }
         oCount = 0;
@@ -149,37 +149,49 @@ function getWinner(board){
     }
     //diagonal
     const topLeft = board[0][0];
-    if (board[1][1] == topLeft && board[2][2] == topLeft && topLeft != '.'){
+    if (board[1][1] == topLeft && board[2][2] == topLeft && topLeft != '.') {
         return topLeft;
     }
     const topRight = board[0][2];
-    if (board[1][1] == topRight && board[2][0] == topRight && topRight != '.'){
+    if (board[1][1] == topRight && board[2][0] == topRight && topRight != '.') {
         return topRight;
     }
     return undefined;
 }
-function getRandomAiCoords(board){
+function getRandomAiCoords(board) {
 
-    while (true){
+    while (true) {
         let aiCoords = [Math.floor(Math.random() * 3), Math.floor(Math.random() * 3)];
-        if (board[aiCoords[0]][aiCoords[1]] == '.'){
+        if (board[aiCoords[0]][aiCoords[1]] == '.') {
             return aiCoords;
         }
     }
 }
 
-function getUnbeatableAiCoords(board){
-    let counter = 0;
-    for (const row of board){
-        if (row[0] == 'o'){
-            for (const column of row){
-                if (column == row[0]){
-                    counter++
-                    if (counter == 2){
-                        return [board.indexOf(row), row.indexOf(column)];
-                    }
+function getUnbeatableAiCoords(board) {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == '.') {
+                board[i][j] = 'o';
+                if (getWinner(board) == 'o') {
+                    board[i][j] = '.';
+                    return [i, j];
                 }
+                board[i][j] = '.';
             }
-        }   
+        }
     }
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] == ".") {
+                board[i][j] = 'x';
+                if (getWinner(board) == 'x') {
+                    board[i][j] = '.';
+                    return [i, j];
+                }
+                board[i][j] = '.';
+            }
+        }
+    }
+    return getRandomAiCoords(board);
 }
